172c3cbedd6ffcd004a258f79756a8fb7385689f
deniseschirmer <deniseschirmerr@gmail.com>
2023-10-03 10:10:57 -0300

    feat: adding structor

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Clone Reddit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+export default function RootLayout({
+  children,
+  authModal,
+}: {
+  children: React.ReactNode;
+  authModal: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        {authModal}
+
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
